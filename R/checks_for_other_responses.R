8d33ab88395f3914503502bd551c2319f1f2841e
Amos Nyarwaya <amos.nyarwaya@gmail.com>
2022-05-20 10:08:39 +0300

    other checks function

---
+++ R/checks_for_other_responses.R
@@ -0,0 +1,98 @@
+# extract others checks function
+
+extract_other_data_an <- function(input_tool_data, input_survey, input_choices) {
+  
+  # add and rename some columns
+  df_data_an <- input_tool_data %>% 
+    rename(uuid = `_uuid`) %>% 
+    mutate(start_date = as_date(start))
+  
+  # get questions with other
+  others_colnames <- df_data_an %>% 
+    select(ends_with("_other"), -contains("/")) %>% 
+    colnames()
+  
+  # data.frame for holding _other response data
+  df_other_response_data_an <- data.frame()
+  
+  for (cln in others_colnames) {
+    
+    current_parent_qn = str_replace_all(string = cln, pattern = "_other", replacement = "")
+    
+    df_filtered_data_an <- df_data_an %>% 
+      select(-contains("/")) %>% 
+      select(uuid, start_date, enumerator_id, district_name, point_number, other_text = cln, current_value = current_parent_qn) %>% 
+      filter(!is.na(other_text), !other_text %in% c("", "NA")) %>%
+      mutate(other_name = cln,
+             int.my_current_val_extract = if_else(str_detect(current_value, "other\\b"), str_extract_all(string = current_value, pattern = "other\\b|[a-z]+._other\\b"), current_value),
+             value = "",
+             parent_qn = current_parent_qn)
+    df_other_response_data_an <- rbind(df_other_response_data_an, df_filtered_data_an)
+  }  
+  
+  #arrange the data
+  df_data_arranged_an <- df_other_response_data_an %>% 
+    arrange(start_date, uuid)
+  
+  # get choices to add to the other responses extracted
+  df_grouped_choices_an <- input_choices %>% 
+    group_by(list_name) %>% 
+    summarise(choice_options = paste(name, collapse = ":")) %>% 
+    arrange(list_name)
+  
+  # extract parent question and join survey for extracting list_name
+  df_data_parent_qns_an <- df_data_arranged_an %>% 
+    left_join(input_survey %>% select(name, type), by = c("parent_qn" = "name")) %>% 
+    seperate(col = type, into = c("select_type", "list_name"), sep = "", remove = TRUE, extra = "drop") %>% 
+    rename(name = parent_qn)
+  
+  # join other responses with choice options based on list_name
+  df_join_other_responses_with_choices <- df_data_parent_qns_an %>% 
+    left_join(df_grouped_choices_an, by = "list_name") %>% 
+    mutate(issue_id = "other_checks",
+           issue = "",
+           checked_by = "",
+           checked_date = as_date(today()),
+           comment = "",
+           reviewed = "",
+           adjust_log = ""
+    ) %>% 
+    filter(str_detect(string = current_value, pattern = "\\bother\\b|\\w+_other\\b"))
+  
+  # care for select_one and select_multiple (change_response, add_option, remove_option)
+  output <- list()
+  # select_one checks
+  output$select_one <- df_join_other_responses_with_choices %>% 
+    filter(str_detect(select_type, c("select_one|select one"))) %>% 
+    mutate(type = "change_response")
+  
+  # select_multiple checks
+  select_mu_data <- df_join_other_responses_with_choices %>% 
+    filter(str_detect(select_type, c("select_multiple|select multiple")))
+  
+  select_mu_add_option <- select_mu_data %>% 
+    mutate(type = "add_option")
+  select_mu_remove_option <- select_mu_data %>% 
+    mutate(type = "remove_option",
+           value = as.character(int.my_current_val_extract))
+  
+  output$select_multiple <- bind_rows(select_mu_add_option, select_mu_remove_option) %>% 
+    arrange(uuid, start_date, enumerator_id, name)
+  
+  # merge other checks
+  merged_other_checks <- bind_rows(output) %>% 
+    mutate(uuid_cl = paste(uuid, "_", type, "_", name),
+           so_sm_choices = choice_options) %>% 
+    select(uuid, start_date, enumerator_id, district_name, point_number, type, name, current_value, value, issue_id, issue, other_text, checked_by, checked_date, comment, reviewed, adjust_log, uuid_cl, so_sm_choices)
+}
+
+
+
+
+
+
+
+
+
+
+
